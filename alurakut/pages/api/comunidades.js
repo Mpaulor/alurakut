f6ade570d87a7284f158e0571fc1fc1eeb8e3521
mpaulor <mpaulor@gmail.com>
2021-07-15 22:35:53 -0300

    Aula 4

---
+++ alurakut/pages/api/comunidades.js
@@ -0,0 +1,29 @@
+import { SiteClient } from 'datocms-client';
+
+export default async function recebedorDeRequests(request, response) {
+    if(request.method === 'POST') {
+        const TOKEN = '10bbf68c2e9c6b49820e1e765d2c4c';
+        const client = new SiteClient(TOKEN);
+        
+        // Validar os dados, antes de sair cadastrando
+        const registroCriado = await client.items.create({
+            itemType: "972785", // ID do Model de "Communities" criado pelo Dato
+            ...request.body,
+            // title: "Comunidade de Teste",
+            // imageUrl: "https://github.com/omariosouto.png",
+            // creatorSlug: "omariosouto"
+        })
+    
+        console.log(registroCriado);
+    
+        response.json({
+            dados: 'Algum dado qualquer',
+            registroCriado: registroCriado,
+        })
+        return;
+    }
+
+    response.status(404).json({
+        message: 'Não temos nada ainda no GET, mas no POST tem!'
+    })
+}
